319ae8ee154750309b9af19fff55543eca3b1895
Gokulc2002 <gokul@thirvusoft.in>
2021-12-17 12:19:26 +0530

    filter using query

--- thirvu_soft/thirvu_soft/doctype/employee_payroll/employee_payroll.py
+++ thirvu_soft/thirvu_soft/doctype/employee_payroll/employee_payroll.py
@@ -6,7 +6,7 @@ from frappe.model.document import Document
 
 class employee_payroll(Document):
 	 def validate(self):
-		 r={"Developer":1000, "Tech lead":30000, "House keeping":9000, "Intern":8100, "HR":40000}
+		 r={"Developer":21000, "Tech lead":30000, "House keeping":9000, "Intern":8100, "HR":40000}
 		 self.basic_pay=r[self.emp_role]
 		 self.pf=8.5
 
